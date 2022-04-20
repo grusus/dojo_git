925a67d8ab8ef83d0b8b771bb23c130026fb8617
Hubert Zycinski <hzycinski@gmail.com>
2022-04-20 09:45:16 +0200

    Implement Joke method

--- Gittie.cs
+++ Gittie.cs
@@ -2,3 +2,9 @@ public void Introduce()
 {
     Console.WriteLine("Hello, I'm Gittie!");
 }
+
+public void Joke()
+{
+    Console.Writeline("How do you make holy water?");
+    Console.Writeline("You boil the hell out of it!");
+}
